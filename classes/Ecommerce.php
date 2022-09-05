083fe59e08bad6eb9634615d21c4b8b96a0cdb31
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-05 15:17:57 +0200

    Implementazione classe "Ecommerce"

---
+++ classes/Ecommerce.php
@@ -0,0 +1,17 @@
+<?php
+class Ecommerce{
+
+    protected $image;
+    protected $name;
+    protected $brand;
+    protected $price;
+
+    public function __construct($_image, $_name, $_brand, $_price)
+    {
+        $this->image = $_image;
+        $this->name = $_name;
+        $this->brand = $_brand;
+        $this->price = $_price;
+    }
+
+}
